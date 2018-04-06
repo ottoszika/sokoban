36de45e666fdc7d92c1621b3460e96898de1f115
Otto Szika <ottoszika@users.noreply.github.com>
2018-04-06 11:36:17 +0300

    Initial commit

---
+++ desktop/src/com/ottoszika/sokoban/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ottoszika.sokoban.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ottoszika.sokoban.Sokoban;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sokoban(), config);
+	}
+}
